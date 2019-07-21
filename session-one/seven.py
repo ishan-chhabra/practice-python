13c4ac63a8edd5e22fece2b03fbbfa6372503e87
Ishan Chhabra <ishan.chhabra@hotmail.com>
2019-07-21 13:29:13 +0530

    add seven.py

--- session-one/seven.py
+++ session-one/seven.py
@@ -1,7 +1,16 @@
+'''
+    File name: seven.py
+    Author: Ishan Chhabra
+    Date created: 7/20/2019
+    Python Version: 3.7
+'''
 
-
+# Initializing variables...
 rows, cols = (int(input()), int(input()))
 count = 1
+matrix = []
+
+# Fn to print matrix...
 
 
 def printMatrix(matrix):
@@ -9,8 +18,7 @@ def printMatrix(matrix):
         print(" ".join(map(str, row)))
 
 
-matrix = []
-
+# Generating a matrix for given rows and columns...
 for r in range(rows):
     column = []
     for c in range(cols):
@@ -18,4 +26,5 @@ for r in range(rows):
         count = count + 1
     matrix.append(column)
 
+# Printing it out to the user...
 printMatrix(matrix)
